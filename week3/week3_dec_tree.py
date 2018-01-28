0fbc49edc222aab9af8cf8c5912e3aff769d24dd
veksel1 <35739303+veksel1@users.noreply.github.com>
2018-01-28 22:03:28 +0200

    finished week3 classification 1st task and almost 2nd task

--- week3/week3_dec_tree.py
+++ week3/week3_dec_tree.py
@@ -66,24 +66,70 @@ risky_loans_raw = loans[loans[target] == -1]
 print("Number of safe loans  : %s" % len(safe_loans_raw))
 print("Number of risky loans : %s" % len(risky_loans_raw))
 
-#making datasets balanced, merging them and splitting to train_data and validation_data
+#making datasets balanced, merging them, adding dummies for categorical variables 
+#and splitting to train_data and validation_data
 riskyToSafeRatio = len(risky_loans_raw)/len(safe_loans_raw)
 risky_loans = risky_loans_raw
 safe_loans=safe_loans_raw.sample(frac=riskyToSafeRatio,random_state=1) 
 loans_data = risky_loans.append(safe_loans)
+loans_data_with_dummies = pd.get_dummies(loans_data)
 
-train_data, validation_data = train_test_split(loans_data, test_size=0.2, random_state=1)
+train_data, validation_data = train_test_split(loans_data_with_dummies, test_size=0.2, random_state=1)
 
 
 #training the decision tree classifier
 decision_tree_model = tree.DecisionTreeClassifier()
 small_model = tree.DecisionTreeClassifier(max_depth=2)
-np_train_data = np.array(train_data)
-np_validation_data = np.array(validation_data)
-np_target = np.array(train_data[target])
+#np_train_data = np.array(train_data)
+#np_validation_data = np.array(validation_data)
+#np_target = np.array(train_data[target])
 
-decision_tree_model.fit(train_data,train_data[target])
-small_model.fit(np_train_data,np_target)
+train_data_wo_target = train_data[train_data.columns.difference([target])]
+decision_tree_model.fit(train_data_wo_target,train_data[target])
+small_model.fit(train_data_wo_target,train_data[target])
+
+#getting samples for predictions
+validation_safe_loans = validation_data[validation_data[target] == 1]
+validation_risky_loans = validation_data[validation_data[target] == -1]
+
+sample_validation_data_risky = validation_risky_loans[0:2]
+sample_validation_data_safe = validation_safe_loans[0:2]
+
+sample_validation_data = sample_validation_data_safe.append(sample_validation_data_risky)
+sample_validation_data
+sample_validation_data_wo_target = sample_validation_data[sample_validation_data.columns.difference([target])]
+
+#making predictions
+
+decision_tree_model.predict(sample_validation_data_wo_target)
+small_model.predict(sample_validation_data_wo_target)
+
+decision_tree_model.predict_proba(sample_validation_data_wo_target)
+small_model.predict_proba(sample_validation_data_wo_target)
+
+decision_tree_model.score(train_data_wo_target,train_data[target])
+small_model.score(train_data_wo_target,train_data[target])
+
+validation_data_wo_target = validation_data[validation_data.columns.difference([target])]
+
+decision_tree_model.score(validation_data_wo_target,validation_data[target])
+small_model.score(validation_data_wo_target,validation_data[target])
+
+#finding optimal model
+for depth in range(1,100):
+    optimal_model = tree.DecisionTreeClassifier(max_depth=depth)
+    optimal_model.fit(train_data_wo_target,train_data[target])
+    print('depth =', depth)
+    print('train accuracy:',optimal_model.score(train_data_wo_target,train_data[target]))
+    print('valid accuracy:',optimal_model.score(validation_data_wo_target,validation_data[target]))
+    print()
+
+#majority class estimator
+print('train majority class accuracy:', len(train_data[train_data[target]==+1])/len(train_data))
+print('valid majority class accuracy:', len(validation_data[validation_data[target]==+1])/len(validation_data))
+
+
+
 
 ##running regressions
 log_reg = linear_model.LogisticRegression()
